12aa68397818d9dc77a2c105df47833616e4d776
Varun Choudhary <92911349+Maverick101321@users.noreply.github.com>
2024-03-26 23:47:09 +0530

    Add files via upload

---
+++ 1.R
@@ -0,0 +1,19 @@
+a=25
+b<-a*10
+d=a*10
+print(c(a,b))
+print(a)
+print(b)
+print(d)
+# source shows the o/p of the whole program
+# source with echo gives the o/p of the whole program as well as the code
+# in the run command, only the line on which the cursor is present runs!!
+# run cmd can be used to execute selected lines
+# advantages of run cmd - troubleshooting/debugging
+# disadvantages - for large section, console will be populated and messy
+# comments are used because
+# improve readability of the code, to explain the purpose of the code or explain algorithms used for the purpose
+# to generate documentation
+# control + L to clear the console
+# rm(variable_name) to remove a specific variable
+# rm(list=ls()) to clear the environment tab
